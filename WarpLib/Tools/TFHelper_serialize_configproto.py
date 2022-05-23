66445900c2ec9ca34397b72b14bafa3c25b8a6f5
Juraj Ahel <juraj.ahel@gmail.com>
2022-05-23 16:07:33 +0200

    change solution name, remove .NET Framework and GPU acceleration stuff and extra binaries

--- WarpLib/Tools/TFHelper_serialize_configproto.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-gpu_opts = tf.GPUOptions(allow_growth=True)
-config = tf.ConfigProto(gpu_options=gpu_opts)
-config.allow_soft_placement = True
-serialized = config.SerializeToString()
-print(list(map(hex, serialized)))
